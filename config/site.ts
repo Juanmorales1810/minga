3ddce73e8cf4d03381e583ece916ec830b199439
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-07-08 19:47:09 -0300

    Fix: Se cambio el titulo de y la descripción de la pagina

--- config/site.ts
+++ config/site.ts
@@ -1,69 +1,70 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
-  navItems: [
-    {
-      label: "Home",
-      href: "/",
+    name: "Minga",
+    description:
+        "En Minga, nos especializamos en el diseño y la construcción de casas únicas en entornos naturales impresionantes. Nuestro compromiso es brindarte un hogar que no solo sea estético y funcional sino que también armonice con el paisaje, respetando la belleza y la integridad del medio ambiente. Desde La Serena, Chile, hasta cualquier rincón del país, estamos aquí para construir el hogar de tus sueños.",
+    navItems: [
+        {
+            label: "Home",
+            href: "/",
+        },
+        {
+            label: "Docs",
+            href: "/docs",
+        },
+        {
+            label: "Pricing",
+            href: "/pricing",
+        },
+        {
+            label: "Blog",
+            href: "/blog",
+        },
+        {
+            label: "About",
+            href: "/about",
+        },
+    ],
+    navMenuItems: [
+        {
+            label: "Profile",
+            href: "/profile",
+        },
+        {
+            label: "Dashboard",
+            href: "/dashboard",
+        },
+        {
+            label: "Projects",
+            href: "/projects",
+        },
+        {
+            label: "Team",
+            href: "/team",
+        },
+        {
+            label: "Calendar",
+            href: "/calendar",
+        },
+        {
+            label: "Settings",
+            href: "/settings",
+        },
+        {
+            label: "Help & Feedback",
+            href: "/help-feedback",
+        },
+        {
+            label: "Logout",
+            href: "/logout",
+        },
+    ],
+    links: {
+        github: "https://github.com/nextui-org/nextui",
+        twitter: "https://twitter.com/getnextui",
+        docs: "https://nextui.org",
+        discord: "https://discord.gg/9b6yyZKmH4",
+        sponsor: "https://patreon.com/jrgarciadev",
     },
-    {
-      label: "Docs",
-      href: "/docs",
-    },
-    {
-      label: "Pricing",
-      href: "/pricing",
-    },
-    {
-      label: "Blog",
-      href: "/blog",
-    },
-    {
-      label: "About",
-      href: "/about",
-    },
-  ],
-  navMenuItems: [
-    {
-      label: "Profile",
-      href: "/profile",
-    },
-    {
-      label: "Dashboard",
-      href: "/dashboard",
-    },
-    {
-      label: "Projects",
-      href: "/projects",
-    },
-    {
-      label: "Team",
-      href: "/team",
-    },
-    {
-      label: "Calendar",
-      href: "/calendar",
-    },
-    {
-      label: "Settings",
-      href: "/settings",
-    },
-    {
-      label: "Help & Feedback",
-      href: "/help-feedback",
-    },
-    {
-      label: "Logout",
-      href: "/logout",
-    },
-  ],
-  links: {
-    github: "https://github.com/nextui-org/nextui",
-    twitter: "https://twitter.com/getnextui",
-    docs: "https://nextui.org",
-    discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev",
-  },
 };
