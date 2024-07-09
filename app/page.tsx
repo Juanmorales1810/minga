641ac52ce7457b7707eb0cbc258b1d0de1d1d558
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-07-09 19:27:38 -0300

    Fix: Cambios en la UI

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { fontMono } from "@/config/fonts";
 import { Image } from "@nextui-org/image";
 import { Link } from "@nextui-org/link";
 import clsx from "clsx";
+import { WhatsAppLogo } from "@/components/icons";
 
 export default function Home() {
   const words = ["mejores", "lindas", "hermosas", "rápidas", "seguras"];
@@ -13,10 +14,10 @@ export default function Home() {
     <section className="flex flex-col items-center justify-center gap-4">
       <section className="relative h-auto min-h-screen w-full">
         <div className="flex justify-center items-center w-full h-[calc(100vh-64px)]">
-          <Image radius="none" src="/img/home/minga-home.webp" alt="logo" width={1920} height={1080} fallbackSrc="https://via.placeholder.com/1920x1080" className="w-[1920px] h-[calc(100vh-64px)] object-cover object-center brightness-[.20]" />
+          <Image radius="none" src="/img/Fotos Minga 2/trenzaestudio-20.webp" alt="logo" width={1920} height={1080} fallbackSrc="https://via.placeholder.com/1920x1080" className="w-[1920px] h-[calc(100vh-64px)] object-cover object-center brightness-[.20]" />
         </div>
         <div className="flex flex-col justify-center items-center z-20 absolute bottom-10 inset-0">
-          <Image src="/logo.png" alt="logo" fallbackSrc="https://via.placeholder.com/300x200" className="w-[300px] h-[200px]" />
+          <Image src="/img/logo/Minga PNG.png" alt="logo" fallbackSrc="https://via.placeholder.com/300x200" className="w-[300px] h-[200px]" />
           <h2 className={title({ class: fontMono.className + " z-20 pt-8 text-center", size: "sm" })}>Descubre tu hogar ideal en la naturaleza con Minga</h2>
           <Button size="sm" as={Link} href="/portfolio" className="bg-[#7CA4DC] mt-4 font-semibold text-black">Contactanos</Button>
         </div>
@@ -32,7 +33,7 @@ export default function Home() {
           <h2 className={title({ class: fontMono.className + " text-center", size: "md" })}>Diseño y Sostenibilidad</h2>
           <p className="text-center text-2xl text-pretty">Explora cómo cada hogar <strong className={fontMono.className}>Minga</strong> combina innovación arquitectónica con sostenibilidad ambiental</p>
         </div>
-        <div className="flex flex-wrap justify-between items-start w-full max-w-5xl gap-6">
+        <div className="flex flex-wrap justify-between items-start w-full max-w-5xl gap-6 pt-4">
           <article className="flex flex-col justify-center items-center gap-2 w-72">
             <header>
               <h3 className={clsx(
@@ -41,9 +42,9 @@ export default function Home() {
               )}>Diseño Personalizado</h3>
             </header>
             <main>
-              <Image src="/logo.png" alt="logo" fallbackSrc="https://via.placeholder.com/300x200" className="w-[300px] h-[200px]" />
+              <Image isBlurred src="/img/Fotos Minga 2/trenzaestudio-3.webp" alt="logo" fallbackSrc="https://via.placeholder.com/300x200" className="w-[300px] h-[200px]" />
             </main>
-            <footer className="text-pretty text-md">
+            <footer className="text-pretty text-md pt-4">
               <p>Cada hogar <strong className={fontMono.className}>Minga</strong> es un reflejo de los sueños y necesidades del propietario. Ofrecemos soluciones personalizadas que se adaptan a diferentes estilos de vida y preferencias personales. Ya sea que busques un diseño moderno o rústico, en entornos urbanos o naturales, tenemos la experiencia para hacerlo realidad.</p>
             </footer>
           </article>
@@ -55,9 +56,9 @@ export default function Home() {
               )}>Construcción Sostenible</h3>
             </header>
             <main>
-              <Image src="/logo.png" alt="logo" fallbackSrc="https://via.placeholder.com/300x200" className="w-[300px] h-[200px]" />
+              <Image isBlurred src="/img/Fotos Minga 2/trenzaestudio-15.webp" alt="logo" fallbackSrc="https://via.placeholder.com/300x200" className="w-[300px] h-[200px]" />
             </main>
-            <footer className="text-pretty text-md">
+            <footer className="text-pretty text-md pt-4">
               <p>Comprometidos con el cuidado del planeta, utilizamos materiales ecológicos y técnicas de construcción que minimizan el impacto ambiental de nuestras edificaciones. Nuestras casas pre fabricadas son un testimonio de eficiencia y calidad.</p>
             </footer>
           </article>
@@ -69,9 +70,9 @@ export default function Home() {
               )}>Integración con la Naturaleza</h3>
             </header>
             <main>
-              <Image src="/logo.png" alt="logo" fallbackSrc="https://via.placeholder.com/300x200" className="w-[300px] h-[200px]" />
+              <Image isBlurred src="/img/Fotos Minga 2/trenzaestudio-9.webp" alt="logo" fallbackSrc="https://via.placeholder.com/300x200" className="w-[300px] h-[200px]" />
             </main>
-            <footer className="text-pretty text-md">
+            <footer className="text-pretty text- pt-4">
               <p>Diseñamos casas que complementan su entorno, maximizando las vistas naturales y fomentando un vínculo único entre tu hogar y la naturaleza circundante.</p>
             </footer>
           </article>
@@ -87,7 +88,85 @@ export default function Home() {
       <section className="relative w-full max-w-2xl mx-auto">
         <GallerySwiper />
       </section>
+      <section className="flex flex-col justify-center items-center w-full max-w-5xl py-8">
+        <h2 className={title({ class: fontMono.className + " text-center", size: "md" })}>Lo que dicen nuestros clientes</h2>
+        <p className="text-2xl text-center w-full max-w-2xl pt-6">Escucha directamente a aquellos que han convertido su visión en realidad con nosotros y ahora disfrutan de la vida en sus hogares <strong className={fontMono.className}>Minga</strong>.</p>
+        <div className="relative flex justify-center items-center w-full max-w-5xl h-96">
+          <div className="absolute left-0 bottom-5">
+            <Image src="/img/Fotos Minga/Fotografías Oficiales_-17.webp" alt="logo" fallbackSrc="https://via.placeholder.com/300x200" className="w-[300px] h-[200px]" />
+          </div>
+          <q className="z-20 text-2xl w-96 text-center font-mono">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa veniam non aperiam cumque suscipit porro molestias dolorum vitae a ducimus.</q>
+          <div className="absolute right-0 top-5">
+            <Image src="/img/home/istockphoto.jpg" alt="logo" fallbackSrc="https://via.placeholder.com/300x200" className="w-[300px] h-[200px]" />
+          </div>
+        </div>
+        <div className="relative flex justify-center items-center w-full max-w-5xl h-96">
+          <div className="absolute left-0 top-5">
+            <Image src="/img/Fotos Minga/Fotografías Oficiales_-17.webp" alt="logo" fallbackSrc="https://via.placeholder.com/300x200" className="w-[300px] h-[200px]" />
+          </div>
+          <q className="z-20 text-2xl w-96 text-center font-mono">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa veniam non aperiam cumque suscipit porro molestias dolorum vitae a ducimus.</q>
+          <div className="absolute right-0 bottom-5">
+            <Image src="/img/home/istockphoto.jpg" alt="logo" fallbackSrc="https://via.placeholder.com/300x200" className="w-[300px] h-[200px]" />
+          </div>
+        </div>
+        <div className="relative flex justify-center items-center w-full max-w-5xl h-96">
+          <div className="absolute left-0 bottom-5">
+            <Image src="/img/Fotos Minga/Fotografías Oficiales_-17.webp" alt="logo" fallbackSrc="https://via.placeholder.com/300x200" className="w-[300px] h-[200px]" />
+          </div>
+          <q className="z-20 text-2xl w-96 text-center font-mono">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa veniam non aperiam cumque suscipit porro molestias dolorum vitae a ducimus.</q>
+          <div className="absolute right-0 top-5">
+            <Image src="/img/home/istockphoto.jpg" alt="logo" fallbackSrc="https://via.placeholder.com/300x200" className="w-[300px] h-[200px]" />
+          </div>
+        </div>
+      </section>
+      <section className="flex flex-col justify-center items-center gap-4 py-8">
+        <h2 className={title({ class: fontMono.className + " text-center pb-6", size: "md" })}>Tu casa en tres pasos</h2>
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-row justify-center items-center w-full max-w-4xl">
+            <p className="basis-16 text-9xl font-bold text-zinc-50/50">1</p>
+            <div className="basis-2/3">
+              <h3 className="text-2xl pb-4">Consulta Inicial</h3>
+              <p className="text-md text-pretty">Comienza tu viaje hacia el hogar perfecto poniéndote en contacto con nosotros a través de WhatsApp o nuestro formulario en línea. Cuéntanos qué buscas en tu casa ideal y dónde te gustaría construirla. Desde La Serena hasta cualquier lugar de Chile, estamos listos para ayudarte.</p>
+            </div>
+          </div>
+          <div className="flex flex-row-reverse justify-center items-center w-full max-w-4xl">
+            <p className="basis-16 text-9xl font-bold text-zinc-50/50">2</p>
+            <div className="basis-2/3">
+              <h3 className="text-2xl pb-4">Diseño Personalizado</h3>
+              <p className="text-md text-pretty">Trabaja junto a nuestros expertos diseñadores para esbozar y luego perfeccionar el diseño de tu nueva casa, asegurando que cada detalle cumpla con tus expectativas. Nos especializamos en casas pre fabricadas que son tanto estéticas como funcionales.</p>
+            </div>
+          </div>
+          <div className="flex flex-row justify-center items-center w-full max-w-4xl">
+            <p className="basis-16 text-9xl font-bold text-zinc-50/50">3</p>
+            <div className="basis-2/3">
+              <h3 className="text-2xl pb-4">Construcción y Entrega</h3>
+              <p className="text-md text-pretty">Deja en nuestras manos la construcción de tu hogar. Nos encargamos de todo el proceso hasta que te entregamos las llaves de tu nueva casa, lista para ser habitada. La calidad de construcción y el diseño están garantizados.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="py-8">
+        <div className="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+          <h5 className={clsx(fontMono.className, "mb-2 text-3xl font-bold text-gray-900 dark:text-white")}>Diseña tu hogar con Minga hoy</h5>
+          <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">Detalles claros sobre cómo contactar a Minga por WhatsApp, correo electrónico y a través de un formulario de contacto en línea.
+          </p>
+          <div className="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4 rtl:space-x-reverse">
+            <Button
+              as={Link}
+              href="https://wa.me/56912345678"
+              className="w-full sm:w-auto bg-green hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-green-700 dark:hover:bg-green-800 dark:focus:ring-green-700"
+              size="lg"
+              startContent={<WhatsAppLogo className="w-7 h-7 fill-white" />}
+            >
+              <div className="text-left rtl:text-right">
+                <div className="mb-1 text-xs">Contáctanos en</div>
+                <div className="-mt-1 font-sans text-sm font-semibold">WhatsApp</div>
+              </div>
+            </Button>
 
+          </div>
+        </div>
+      </section>
     </section>
   );
 }
