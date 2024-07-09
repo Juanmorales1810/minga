a31a78d692227d59a6831a78bfaf2fab9cecac32
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-07-09 19:28:03 -0300

    Fix: Se agregaron las imágenes en la galería

--- components/gallerySwiper.tsx
+++ components/gallerySwiper.tsx
@@ -6,19 +6,77 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/effect-coverflow';
+// import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 import '@/styles/globals.css';
 
 // import required modules
-import { EffectCoverflow, Pagination } from 'swiper/modules';
+import { EffectCoverflow, Navigation, Pagination } from 'swiper/modules';
 import { Image } from '@nextui-org/image';
+
+const slides = [
+    '/img/Fotos Minga 2/trenzaestudio-2.webp',
+    '/img/Fotos Minga 2/trenzaestudio-3.webp',
+    '/img/Fotos Minga 2/trenzaestudio-4.webp',
+    '/img/Fotos Minga 2/trenzaestudio-5.webp',
+    '/img/Fotos Minga 2/trenzaestudio-6.webp',
+    '/img/Fotos Minga 2/trenzaestudio-7.webp',
+    '/img/Fotos Minga 2/trenzaestudio-8.webp',
+    '/img/Fotos Minga 2/trenzaestudio-9.webp',
+    '/img/Fotos Minga 2/trenzaestudio-10.webp',
+    '/img/Fotos Minga 2/trenzaestudio-11.webp',
+    '/img/Fotos Minga 2/trenzaestudio-12.webp',
+    // '/img/Fotos Minga 2/trenzaestudio-13.webp',
+    '/img/Fotos Minga 2/trenzaestudio-14.webp',
+    '/img/Fotos Minga 2/trenzaestudio-15.webp',
+    '/img/Fotos Minga 2/trenzaestudio-16.webp',
+    '/img/Fotos Minga 2/trenzaestudio-17.webp',
+    '/img/Fotos Minga 2/trenzaestudio-18.webp',
+    // '/img/Fotos Minga 2/trenzaestudio-19.webp',
+    '/img/Fotos Minga 2/trenzaestudio-20.webp',
+    // '/img/Fotos Minga 2/trenzaestudio-21.webp',
+    // '/img/Fotos Minga 2/trenzaestudio-22.webp',
+    '/img/Fotos Minga 2/trenzaestudio-23.webp',
+    '/img/Fotos Minga/Fotografías Oficiales_.webp',
+    '/img/Fotos Minga/Fotografías Oficiales_-2.webp',
+    // '/img/Fotos Minga/Fotografías Oficiales_-3.webp',
+    '/img/Fotos Minga/Fotografías Oficiales_-4.webp',
+    // '/img/Fotos Minga/Fotografías Oficiales_-5.webp',
+    '/img/Fotos Minga/Fotografías Oficiales_-6.webp',
+    '/img/Fotos Minga/Fotografías Oficiales_-7.webp',
+    '/img/Fotos Minga/Fotografías Oficiales_-8.webp',
+    '/img/Fotos Minga/Fotografías Oficiales_-9.webp',
+    '/img/Fotos Minga/Fotografías Oficiales_-10.webp',
+    '/img/Fotos Minga/Fotografías Oficiales_-11.webp',
+    // '/img/Fotos Minga/Fotografías Oficiales_-12.webp',
+    '/img/Fotos Minga/Fotografías Oficiales_-13.webp',
+    '/img/Fotos Minga/Fotografías Oficiales_-14.webp',
+    '/img/Fotos Minga/Fotografías Oficiales_-15.webp',
+    '/img/Fotos Minga/Fotografías Oficiales_-16.webp',
+    '/img/Fotos Minga/Fotografías Oficiales_-17.webp',
+    '/img/Fotos Minga/Fotografías Oficiales_-18.webp',
+    '/img/Fotos Minga/Fotografías Oficiales_-19.webp',
+    // '/img/Fotos Minga/Fotografías Oficiales_-20.webp',
+    '/img/Fotos Minga/Fotografías Oficiales_-21.webp',
+    '/img/Fotos Minga/Fotografías Oficiales_-22.webp',
+    '/img/Fotos Minga/Fotografías Oficiales_-23.webp',
+    // '/img/Fotos Minga/Fotografías Oficiales_-24.webp',
+    '/img/Fotos Minga/Fotografías Oficiales_-25.webp',
+    '/img/Fotos Minga/Fotografías Oficiales_-26.webp',
+];
 
 export default function GallerySwiper() {
     return (
         <>
             <Swiper
+                style={{
+                    //@ts-ignore
+                    '--swiper-navigation-size': '30px',
+                    '--swiper-navigation-color': '#fff',
+                    '--swiper-pagination-color': '#fff'
+                }}
                 effect={'coverflow'}
                 grabCursor={true}
                 centeredSlides={true}
@@ -30,37 +88,16 @@ export default function GallerySwiper() {
                     modifier: 1,
                     slideShadows: true,
                 }}
+                navigation={true}
                 pagination={true}
-                modules={[EffectCoverflow, Pagination]}
+                modules={[EffectCoverflow, Pagination, Navigation]}
                 className="mySwiper"
             >
-                <SwiperSlide>
-                    <Image src="https://swiperjs.com/demos/images/nature-1.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image src="https://swiperjs.com/demos/images/nature-2.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image src="https://swiperjs.com/demos/images/nature-3.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image src="https://swiperjs.com/demos/images/nature-4.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image src="https://swiperjs.com/demos/images/nature-5.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image src="https://swiperjs.com/demos/images/nature-6.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image src="https://swiperjs.com/demos/images/nature-7.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image src="https://swiperjs.com/demos/images/nature-8.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image src="https://swiperjs.com/demos/images/nature-9.jpg" />
-                </SwiperSlide>
+                {slides.map((slide, index) => (
+                    <SwiperSlide key={index}>
+                        <Image src={slide} width={700} height={700} className='w-[700px] h-[700px]' />
+                    </SwiperSlide>
+                ))}
             </Swiper>
         </>
     );
