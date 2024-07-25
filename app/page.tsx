e742cb213d28617e1252b7f244b4fc0bea2dadfe
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-07-25 13:01:14 -0300

    Fix: Cambios en la UI en el responsive del texto

--- app/page.tsx
+++ app/page.tsx
@@ -12,28 +12,28 @@ export default function Home() {
           <Image radius="none" src="/img/Fotos Minga/Fotografías Oficiales_-5.webp" alt="logo" width={1920} height={1080} fallbackSrc="https://via.placeholder.com/1920x1080" className="w-screen h-screen object-cover object-center brightness-75" />
         </div>
         <div className="flex flex-col justify-around items-end z-20 w-full h-screen px-6 absolute bottom-10 inset-0 md:pr-44">
-          <Image radius="none" src="/img/logo/MINGA LOGO NEGRO PNG.png" alt="logo" fallbackSrc="https://via.placeholder.com/300x200" className="w-[150px] h-[100px] md:w-[300px] md:h-[200px] " />
+          <Image radius="none" src="/img/logo/MINGA LOGO NEGRO PNG.png" alt="logo" fallbackSrc="https://via.placeholder.com/300x200" className="w-[150px] h-[100px] md:w-[200px] md:h-[150px] " />
           <div className="flex flex-col items-end md:w-1/3">
-            <h2 className="z-20 pt-8 pl-6 text-3xl font-bold text-right">Viviendas diseñadas para ofrecer confort y estilo en cada rincón</h2>
+            <h2 className="z-20 pt-8 pl-6 text-xl font-bold text-right md:text-3xl">Viviendas diseñadas para ofrecer confort y estilo en cada rincón</h2>
             <Button variant="shadow" endContent={<WhatsAppLogo className="w-6 h-6 fill-white" />} size="md" as={Link} href="https://wa.me/+56940006843" className="mt-4 font-bold w-44 bg-zinc-950">Contactanos</Button>
           </div>
         </div>
       </section>
-      <section className="flex justify-center items-center px-4 h-screen">
+      <section className="flex justify-center items-center px-8 h-screen">
         <div className="flex flex-col justify-center items-end w-full max-w-3xl pl-16 gap-4 text-zinc-950">
           {/* <h1 className={title({ class: fontMono.className + " text-right", size: "md" })}>Bienvenido a Minga</h1> */}
-          <p className="text-right text-4xl text-pretty font-bold pb-6">En Minga, nos especializamos en el diseño y la construcción de casas únicas en entornos naturales impresionantes.</p>
-          <p className="text-right text-2xl text-pretty font-medium text-zinc-700">Nuestro compromiso es brindarte un hogar que no solo sea estético y funcional sino que también armonice con el paisaje, respetando la belleza y la integridad del medio ambiente. Desde La Serena, Chile, hasta cualquier rincón del país, estamos aquí para construir el hogar de tus sueños</p>
+          <p className="text-right text-xl text-pretty font-bold pb-6 sm:text-2xl md:text-4xl">En Minga, nos especializamos en el diseño y la construcción de casas únicas en entornos naturales impresionantes.</p>
+          <p className="text-right text-lg text-pretty font-medium text-zinc-700 sm:text-xl md:text-2xl">Nuestro compromiso es brindarte un hogar que no solo sea estético y funcional sino que también armonice con el paisaje, respetando la belleza y la integridad del medio ambiente. Desde La Serena, Chile, hasta cualquier rincón del país, estamos aquí para construir el hogar de tus sueños</p>
         </div>
       </section>
       <section className="relative flex items-end w-full h-screen">
         <Image radius="none" src="/img/Fotos Minga 2/trenzaestudio-19.webp" alt="logo" fallbackSrc="https://via.placeholder.com/1920x1080" className="w-screen h-screen object-cover object-center" />
         <div className="flex flex-col justify-around items-end z-20 w-full h-screen px-6 absolute bottom-10 inset-0 md:pr-44">
           <div className="flex flex-col items-end md:w-1/3">
-            <p className="z-20 pt-8 text-right text-4xl font-bold">Construcción y diseño de casas desde la región de Coquimbo a todo Chile</p>
+            <p className="z-20 pt-8 text-right text-xl font-bold sm:text-2xl md:text-4xl">Construcción y diseño de casas desde la región de Coquimbo a todo Chile</p>
           </div>
           <div className="flex flex-col items-end md:w-1/3">
-            <p className="z-20 pt-8 text-right text-2xl text-zinc-50">Explora cómo cada hogar Minga combina innovación arquitectónica con sostenibilidad ambiental.
+            <p className="z-20 pt-8 text-right text-lg text-zinc-50 sm:text-xl md:text-2xl">Explora cómo cada hogar Minga combina innovación arquitectónica con sostenibilidad ambiental.
             </p>
             <Button variant="shadow" endContent={<WhatsAppLogo className="w-6 h-6 fill-white" />} size="md" as={Link} href="https://wa.me/+56940006843" className="mt-4 font-bold w-44 bg-zinc-950">Contactanos</Button>
           </div>
@@ -42,22 +42,22 @@ export default function Home() {
       </section>
       <section className="flex flex-col justify-center items-center w-full text-zinc-950">
         <div className="flex flex-col w-full md:flex-row">
-          <div className="flex flex-col justify-center items-center w-full px-4 h-screen">
+          <div className="flex flex-col justify-center items-center w-full px-8 h-screen">
             <Image className="w-36 h-36 mb-8" src="/img/icons/ICONO PREGUNTAS.svg" />
             <div className="z-10 max-w-4xl">
-              <p className="text-2xl text-center font-semibold text-pretty">Comienza tu viaje hacia el hogar perfecto poniéndote en contacto con nosotros a través de WhatsApp o nuestro formulario en línea. Cuéntanos qué buscas en tu casa ideal y dónde te gustaría construirla. Desde La Serena hasta cualquier lugar de Chile, estamos listos para ayudarte.</p>
+              <p className="text-xl text-center font-semibold text-pretty sm:text-2xl">Comienza tu viaje hacia el hogar perfecto poniéndote en contacto con nosotros a través de WhatsApp o nuestro formulario en línea. Cuéntanos qué buscas en tu casa ideal y dónde te gustaría construirla. Desde La Serena hasta cualquier lugar de Chile, estamos listos para ayudarte.</p>
             </div>
           </div>
-          <div className="flex flex-col justify-center items-center w-full px-4 h-screen bg-zinc-950">
+          <div className="flex flex-col justify-center items-center w-full px-8 h-screen bg-zinc-950">
             <Image className="w-36 h-36 mb-8" src="/img/icons/ICONO DISEÑO.svg" />
             <div className="z-10 max-w-4xl">
-              <p className="text-2xl text-center font-semibold text-pretty text-zinc-50">Trabaja junto a nuestros expertos diseñadores para esbozar y luego perfeccionar el diseño de tu nueva casa, asegurando que cada detalle cumpla con tus expectativas. Nos especializamos en casas pre fabricadas que son tanto estéticas como funcionales.</p>
+              <p className="text-xl text-center font-semibold text-pretty text-zinc-50 sm:text-2xl">Trabaja junto a nuestros expertos diseñadores para esbozar y luego perfeccionar el diseño de tu nueva casa, asegurando que cada detalle cumpla con tus expectativas. Nos especializamos en casas pre fabricadas que son tanto estéticas como funcionales.</p>
             </div>
           </div>
-          <div className="flex flex-col justify-center items-center w-full px-4 h-screen">
+          <div className="flex flex-col justify-center items-center w-full px-8 h-screen">
             <Image className="w-36 h-36 mb-8" src="/img/icons/ICONO 3.svg" />
             <div className="z-10 max-w-4xl">
-              <p className="text-2xl text-center font-semibold text-pretty">Deja en nuestras manos la construcción de tu hogar. Nos encargamos de todo el proceso hasta que te entregamos las llaves de tu nueva casa, lista para ser habitada. La calidad de construcción y el diseño están garantizados.</p>
+              <p className="text-xl text-center font-semibold text-pretty sm:text-2xl">Deja en nuestras manos la construcción de tu hogar. Nos encargamos de todo el proceso hasta que te entregamos las llaves de tu nueva casa, lista para ser habitada. La calidad de construcción y el diseño están garantizados.</p>
             </div>
           </div>
         </div>
@@ -70,8 +70,8 @@ export default function Home() {
         <Image radius="none" src="/img/Fotos Minga/Fotografías Oficiales_-23.webp" alt="logo" fallbackSrc="https://via.placeholder.com/1920x1080" className="w-screen h-screen object-cover object-center hidden md:block" />
         <div className="flex flex-col justify-around items-end z-20 w-full h-screen px-6 absolute bottom-10 inset-0">
           <div className="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-black/70 backdrop-blur-sm dark:border-zinc-700/30">
-            <h4 className="mb-2 text-3xl font-bold text-zinc-50">Diseña tu hogar con Minga hoy</h4>
-            <p className="mb-5 text-base text-zinc-50 sm:text-lg">Detalles claros sobre cómo contactar a Minga por WhatsApp, correo electrónico y a través de un formulario de contacto en línea.
+            <h4 className="mb-2 text-2xl font-bold text-zinc-50 sm:text-3xl">Diseña tu hogar con Minga hoy</h4>
+            <p className="mb-5 text-md text-zinc-50 sm:text-lg">Detalles claros sobre cómo contactar a Minga por WhatsApp, correo electrónico y a través de un formulario de contacto en línea.
             </p>
             <div className="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4 rtl:space-x-reverse">
               <Button
