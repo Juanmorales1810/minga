4966d2ff20e3b2cfa001548855cd46741a2d6c93
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-08-02 20:50:32 -0300

    feat: Se hicieron cambios en la UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@nextui-org/button";
-import { Image } from "@nextui-org/image";
+// import { Image } from "@nextui-org/image";
 import { Link } from "@nextui-org/link";
-import { LinkedIn, WhatsAppLogo } from "@/components/icons";
+import { InstagramLogo, LinkedIn, MailLogo, MapLogo, WhatsAppLogo } from "@/components/icons";
 import GallerySwiper from "@/components/gallerySwiper";
-import BackgroundSwipe from "@/components/backgroundswipe";
 import { TextGenerateEffect } from "@/components/text-generate-effect";
+import Image from "next/image";
+import { Tooltip } from "@nextui-org/tooltip";
 
 export default function Home() {
   const slides = [
@@ -26,66 +27,84 @@ export default function Home() {
     <section className="flex flex-col items-center w-full justify-center min-h-[calc(100vh-100px)]">
       <section className="relative h-auto min-h-screen w-full">
         <div className="flex justify-center items-center w-full h-screen">
-          <BackgroundSwipe />
+          <Image src="/img/background/Portada.avif" alt="Imagen principal de Minga" width={1920} height={1080} className="w-screen h-screen object-cover object-center" />
         </div>
         <div className="flex flex-col justify-around items-end z-20 w-full h-screen px-6 absolute bottom-10 inset-0 md:pr-44">
-          <Image radius="none" src="/img/logo/MINGA LOGO NEGRO PNG.png" alt="logo" fallbackSrc="https://via.placeholder.com/300x200" className="w-[150px] h-[100px]" />
+          <Image src="/img/logo/MINGA LOGO NEGRO PNG.png" alt="logo" width={150} height={100} className="w-[150px] h-[100px]" />
           <div className="flex flex-col items-end md:w-1/3">
-            <h2 className="z-20 pt-8 pl-6 text-xl font-bold text-right md:text-4xl">Viviendas diseñadas para ofrecer confort y estilo en cada rincón</h2>
-            <Button variant="shadow" endContent={<WhatsAppLogo className="w-6 h-6 fill-white" />} size="md" as={Link} href="https://wa.me/+56940006843" className="mt-4 font-bold w-44 bg-zinc-950">Contactanos</Button>
+            <h1 className="z-20 pt-8 pl-6 text-xl font-bold text-right md:text-4xl">Bienvenido a Minga</h1>
+            <p className="text-right">
+              Diseño y Construcción
+              <br />
+              Desde la IV Región. Chile.
+            </p>
           </div>
         </div>
       </section>
       <section className="flex justify-center items-center px-8 h-screen">
-        <div className="flex flex-col justify-center items-end w-full max-w-3xl pl-16 gap-4 text-zinc-950">
-          <h1 className="text-xl text-right">Bienvenido a Minga</h1>
-          <TextGenerateEffect className="text-right text-xl text-pretty font-bold pb-6 sm:text-2xl md:text-4xl" words="En Minga, nos especializamos en el diseño y la construcción de casas únicas en entornos naturales impresionantes." />
-          <TextGenerateEffect delay={3000} className="text-right text-lg text-pretty font-medium text-zinc-700 sm:text-xl md:text-2xl" words="Nuestro compromiso es brindarte un hogar que no solo sea estético y funcional sino que también armonice con el paisaje, respetando la belleza y la integridad del medio ambiente. Desde La Serena, Chile, hasta cualquier rincón del país, estamos aquí para construir el hogar de tus sueños" />
-        </div>
-      </section>
-      <section className="relative flex items-end w-full h-screen">
-        <Image radius="none" src="/img/Fotos Minga 2/trenzaestudio-19.webp" alt="logo" fallbackSrc="https://via.placeholder.com/1920x1080" className="w-screen h-screen object-cover object-center" />
-        <div className="flex flex-col justify-around items-end z-20 w-full h-screen px-6 absolute bottom-10 inset-0 md:pr-44">
-          <div className="flex flex-col items-end md:w-1/3">
-            <p className="z-20 pt-8 text-right text-xl font-bold sm:text-2xl md:text-4xl">Construcción y diseño de casas desde la región de Coquimbo a todo Chile</p>
-          </div>
-          <div className="flex flex-col items-end md:w-1/3">
-            <p className="z-20 pt-8 text-right text-lg text-zinc-50 sm:text-xl md:text-2xl">Explora cómo cada hogar Minga combina innovación arquitectónica con sostenibilidad ambiental.
-            </p>
-            <Button variant="shadow" endContent={<WhatsAppLogo className="w-6 h-6 fill-white" />} size="md" as={Link} href="https://wa.me/+56940006843" className="mt-4 font-bold w-44 bg-zinc-950">Contactanos</Button>
-          </div>
-        </div>
-
-      </section>
-      <section className="flex flex-col justify-center items-center w-full text-zinc-950">
-        <div className="flex flex-col w-full md:flex-row">
-          <div className="flex flex-col justify-center items-center w-full px-8 h-screen">
-            <Image className="w-36 h-36 mb-8" src="/img/icons/ICONO PREGUNTAS.svg" />
-            <div className="z-10 max-w-4xl">
-              <p className="text-xl text-center font-semibold text-pretty sm:text-2xl">Comienza tu viaje hacia el hogar perfecto poniéndote en contacto con nosotros a través de WhatsApp o nuestro formulario en línea. Cuéntanos qué buscas en tu casa ideal y dónde te gustaría construirla. Desde La Serena hasta cualquier lugar de Chile, estamos listos para ayudarte.</p>
-            </div>
-          </div>
-          <div className="flex flex-col justify-center items-center w-full px-8 h-screen bg-zinc-950">
-            <Image className="w-36 h-36 mb-8" src="/img/icons/ICONO DISEÑO.svg" />
-            <div className="z-10 max-w-4xl">
-              <p className="text-xl text-center font-semibold text-pretty text-zinc-50 sm:text-2xl">Trabaja junto a nuestros expertos diseñadores para esbozar y luego perfeccionar el diseño de tu nueva casa, asegurando que cada detalle cumpla con tus expectativas. Nos especializamos en casas pre fabricadas que son tanto estéticas como funcionales.</p>
-            </div>
-          </div>
-          <div className="flex flex-col justify-center items-center w-full px-8 h-screen">
-            <Image className="w-36 h-36 mb-8" src="/img/icons/ICONO 3.svg" />
-            <div className="z-10 max-w-4xl">
-              <p className="text-xl text-center font-semibold text-pretty sm:text-2xl">Deja en nuestras manos la construcción de tu hogar. Nos encargamos de todo el proceso hasta que te entregamos las llaves de tu nueva casa, lista para ser habitada. La calidad de construcción y el diseño están garantizados.</p>
-            </div>
-          </div>
+        <div className="flex flex-col justify-center items-end w-full max-w-5xl gap-4 md:flex-row-reverse md:justify-between md:items-center">
+          <TextGenerateEffect className="text-white text-right text-xl text-pretty font-bold pb-6 sm:text-2xl md:text-4xl" words="Proyecto de alto estándar con identidad única" />
+          <Button variant="shadow" endContent={<WhatsAppLogo className="w-6 h-6 fill-white" />} as={Link} href="https://wa.me/+56940006843" className="mt-4 font-bold w-72 bg-zinc-950 md:mt-0">Diseña tu hogar con Minga hoy</Button>
         </div>
       </section>
       <section className="w-full md:h-screen mx-auto">
         <GallerySwiper />
       </section>
+
+      <section className="flex justify-center items-center w-full px-8 h-screen">
+        <div className="flex flex-col justify-center items-end w-full max-w-5xl gap-4 pl-36 md:flex-row-reverse md:justify-between md:items-center">
+          <TextGenerateEffect className="text-white text-right text-xl text-pretty font-bold pb-6 sm:text-2xl md:text-4xl" words="Hacemos énfasis en la selección de materiales y la calidad del detalle constructivo" />
+        </div>
+      </section>
+      <section className="relative flex items-end w-full h-screen">
+        <Image src="/img/background/Foto3-compress.webp" alt="Imagen minga de llamado a la acción" width={1920} height={1080} className="w-screen h-screen object-cover object-center brightness-75" />
+        <div className="flex flex-col justify-center items-end z-20 w-full h-screen px-6 absolute bottom-10 inset-0 md:pr-44">
+          <div className="flex flex-col items-end md:w-1/2">
+            <TextGenerateEffect className="text-white text-right text-xl text-pretty font-bold sm:text-2xl md:text-4xl" words="Nos preocupamos de la selección" />
+            <TextGenerateEffect className="text-white text-right text-xl text-pretty font-bold sm:text-2xl md:text-4xl" words="Nada nos inspira que construir un sueño juntos." />
+          </div>
+
+        </div>
+      </section>
       <section className="relative flex items-start w-full h-screen">
-        <Image radius="none" src="/img/Fotos Minga/Fotografías Oficiales_-24.webp" alt="logo" fallbackSrc="https://via.placeholder.com/1920x1080" className="w-screen h-screen object-cover object-center  block md:hidden" />
-        <Image radius="none" src="/img/Fotos Minga/Fotografías Oficiales_-23.webp" alt="logo" fallbackSrc="https://via.placeholder.com/1920x1080" className="w-screen h-screen object-cover object-center hidden md:block" />
-        <div className="flex flex-col justify-around items-end z-20 w-full h-screen px-6 absolute bottom-10 inset-0">
+        <Image src="/img/background/Foto4-compress.webp" alt="logo" width={1920} height={1080} className="w-screen h-screen object-cover object-center" />
+        <div className="flex flex-col justify-center items-end z-20 w-full h-screen px-6 absolute bottom-10 inset-0 md:flex-row-reverse md:justify-between md:items-center md:px-44">
+          <div className="flex flex-col items-end md:w-1/2">
+            <TextGenerateEffect className="text-white text-right text-xl text-pretty font-bold pb-6 sm:text-2xl md:text-4xl" words="Diseña tu hogar con Minga hoy" />
+            <ul className="flex flex-wrap items-center mt-3 gap-6">
+              <li className="p-2 rounded-full bg-black">
+                <Tooltip content="Hablanos al WhatsApp">
+                  <a href="https://wa.me/+56940006843" className="">
+                    <WhatsAppLogo className="w-8 h-8 fill-zinc-50 hover:fill-zinc-400" />
+                  </a>
+                </Tooltip>
+              </li>
+              <li className="p-2 rounded-full bg-black">
+                <Tooltip placement="bottom" content="Conoce nuestro Instagram">
+                  <a href="https://www.instagram.com/ccminga?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" className="">
+                    <InstagramLogo className="w-8 h-8 fill-zinc-50 hover:fill-zinc-400" />
+                  </a>
+                </Tooltip>
+              </li>
+              <li className="p-2 rounded-full bg-black">
+                <Tooltip content="Abrir ubicación en GoogleMaps">
+                  <a href="https://www.google.com/maps/place/CASAS+MINGA/@-30.0271756,-71.2545806,15z/data=!4m6!3m5!1s0x96903123822d4609:0xb14719ff38359ff3!8m2!3d-30.0271756!4d-71.2545806!16s%2Fg%2F11r3djlkg_?entry=ttu" className="">
+                    <MapLogo className="w-8 h-8 stroke-zinc-50 hover:stroke-zinc-400 fill-none" />
+                  </a>
+                </Tooltip>
+              </li>
+              <li className="p-2 rounded-full bg-black">
+                <Tooltip placement="bottom" content="Envíanos un correo">
+                  <a href="mailTo:Contacto@ccminga.cl" className="">
+                    <MailLogo className="w-8 h-8 stroke-zinc-50 hover:stroke-zinc-400 fill-none" />
+                  </a>
+                </Tooltip>
+              </li>
+            </ul>
+          </div>
+          <Button variant="shadow" endContent={<WhatsAppLogo className="w-6 h-6 fill-white" />} as={Link} href="https://wa.me/+56940006843" className="mt-4 font-bold w-72 bg-zinc-950 md:mt-0">Diseña tu hogar con Minga hoy</Button>
+        </div>
+        {/* <div className="flex flex-col justify-around items-end z-20 w-full h-screen px-6 absolute bottom-10 inset-0">
           <div className="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-black/70 backdrop-blur-sm dark:border-zinc-700/30">
             <h4 className="mb-2 text-2xl font-bold text-zinc-50 sm:text-3xl">Diseña tu hogar con Minga hoy</h4>
             <p className="mb-5 text-md text-zinc-50 sm:text-lg">Detalles claros sobre cómo contactar a Minga por WhatsApp, correo electrónico y a través de un formulario de contacto en línea.
@@ -117,7 +136,7 @@ export default function Home() {
               </Button>
             </div>
           </div>
-        </div>
+        </div> */}
       </section>
     </section>
   );
