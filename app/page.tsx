6e11f70b3b0d380eb9f34d420a8c22428a313347
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-08-05 18:41:38 -0300

    Fix: Correcciones en los textos

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
       </section>
       <section className="flex justify-center items-center px-6 w-full h-screen md:px-0">
         <div className="flex flex-col justify-center items-end w-full gap-4 md:flex-row-reverse md:justify-between md:items-center md:px-40">
-          <TextGenerateEffect className="text-white text-right text-xl text-pretty font-bold pb-6 sm:text-2xl md:text-4xl" words="Proyecto de alto estándar con identidad única" />
+          <TextGenerateEffect className="text-white text-right text-xl text-pretty font-bold pb-6 sm:text-2xl md:text-4xl" words="Proyectos de alto estándar con identidad única" />
           <Button variant="shadow" endContent={<WhatsAppLogo className="w-6 h-6 fill-white" />} as={Link} href="https://wa.me/+56940006843" className="mt-4 font-bold w-72 bg-zinc-950 md:mt-0">Diseña tu hogar con Minga hoy</Button>
         </div>
       </section>
@@ -61,7 +61,8 @@ export default function Home() {
         <div className="flex flex-col justify-center items-end z-20 w-full h-screen px-6 absolute bottom-10 inset-0 md:pr-40">
           <div className="flex flex-col items-end md:w-1/2">
             <TextGenerateEffect className="text-white text-right text-xl text-pretty font-bold sm:text-2xl md:text-4xl" words="Nos preocupamos de la selección" />
-            <TextGenerateEffect className="text-white text-right text-xl text-pretty font-bold sm:text-2xl md:text-4xl" words="Nada nos inspira que construir un sueño juntos." />
+            <TextGenerateEffect delay={1000} className="text-white text-right text-xl text-pretty font-bold sm:text-2xl md:text-4xl" words="Nada nos inspira que construir un sueño juntos" />
+            <TextGenerateEffect delay={2400} className="text-white text-right text-xl text-pretty font-bold sm:text-2xl md:text-2xl" words="Nada nos inspira más  que construir un sueño juntos." />
           </div>
 
         </div>
